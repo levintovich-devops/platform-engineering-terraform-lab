dd5f58fed969024818b8c73cf0714fbdd3164129
Vladimir Levintovich <levintovichvlad@gmail.com>
2026-09-15 14:09:25 +0300

    feat: add namespace resource quota

--- infra-modules/k8s-bootstrap/main.tf
+++ infra-modules/k8s-bootstrap/main.tf
@@ -3,3 +3,14 @@ resource "kubernetes_namespace_v1" "this" {
     name = var.namespace_name
   }
 }
+
+resource "kubernetes_resource_quota_v1" "this" {
+  metadata {
+    name      = "namespace-quota"
+    namespace = kubernetes_namespace_v1.this.metadata[0].name
+  }
+
+  spec {
+    hard = var.resource_quota
+  }
+}
--- infra-modules/k8s-bootstrap/variables.tf
+++ infra-modules/k8s-bootstrap/variables.tf
@@ -7,3 +7,9 @@ variable "kubeconfig_path" {
   type        = string
   description = "Path to the kubeconfig file used by the Kubernetes provider."
 }
+
+variable "resource_quota" {
+  type        = map(string)
+  description = "Resource quota limits applied to the namespace."
+}
+
